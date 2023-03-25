620329b8fd901c5263f5777b94e5ca0c8b8d3275
mihalkoff <ushuaya@abv.bg>
2023-03-25 17:48:03 +0200

    Add Footer.js

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return (
+        <section className="container-fluid footer_section">
+            <p>
+                &copy; <span id="displayYear"></span> All Rights Reserved By
+                <a href="https://html.design/">Free Html Templates</a>
+            </p>
+        </section>
+    );
+};
