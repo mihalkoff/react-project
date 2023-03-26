69c20dee6bcef89641b6bd772f52eedb3fb7c030
mihalkoff <ushuaya@abv.bg>
2023-03-26 20:45:35 +0300

    Update Footer.js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,75 @@
 export const Footer = () => {
     return (
-        <section className="container-fluid footer_section">
-            <p>
-                &copy; <span id="displayYear"></span> All Rights Reserved By
-                <a href="https://html.design/">Free Html Templates</a>
-            </p>
-        </section>
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-md-12 col-lg-4">
+                        <div className="footer-logo">
+                            <a className="navbar-brand" href="#">eStartup</a>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-6 col-md-3 col-lg-2">
+                        <div className="list-menu">
+                            <h4>Abou Us</h4>
+                            <ul className="list-unstyled">
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Features item</a></li>
+                                <li><a href="#">Live streaming</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-6 col-md-3 col-lg-2">
+                        <div className="list-menu">
+                            <h4>Abou Us</h4>
+                            <ul className="list-unstyled">
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Features item</a></li>
+                                <li><a href="#">Live streaming</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-6 col-md-3 col-lg-2">
+                        <div className="list-menu">
+                            <h4>Support</h4>
+                            <ul className="list-unstyled">
+                                <li><a href="#">faq</a></li>
+                                <li><a href="#">Editor help</a></li>
+                                <li><a href="#">Contact us</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-6 col-md-3 col-lg-2">
+                        <div className="list-menu">
+                            <h4>Abou Us</h4>
+                            <ul className="list-unstyled">
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Features item</a></li>
+                                <li><a href="#">Live streaming</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="copyrights">
+                <div className="container">
+                    <p>&copy; Copyrights eStartup. All rights reserved.</p>
+                    <div className="credits">
+                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };
