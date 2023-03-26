a2fd6dcf2f5f3fe000e9c62b6e2a5eddb8f613c4
mihalkoff <ushuaya@abv.bg>
2023-03-26 21:38:26 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
 import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header.js/Header";
+import { Home } from './components/Home/Home';
 
 
 function App() {
 
 	return (
 		<div>
+			<Header />
+			<main id="main">
+				<Home />
+			</main>
 			<Footer />
 		</div>
 	);
