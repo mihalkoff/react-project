fe6894653087894553960f1794d7e1da44d81114
mihalkoff <ushuaya@abv.bg>
2023-03-26 21:38:19 +0300

    Update Header.js

--- client/src/components/Header.js/Header.js
+++ client/src/components/Header.js/Header.js
@@ -9,30 +9,12 @@ export const Header = () => {
 
                 <nav id="navbar" className="navbar">
                     <ul>
-                        <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
-                        <li><a className="nav-link scrollto" href="#about-us">About</a></li>
-                        <li><a className="nav-link scrollto" href="#features">Features</a></li>
-                        <li><a className="nav-link scrollto" href="#screenshots">Screenshots</a></li>
-                        <li><a className="nav-link scrollto" href="#team">Team</a></li>
-                        <li><a className="nav-link scrollto" href="#pricing">Pricing</a></li>
-                        <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
-                            <ul>
-                                <li><a href="#">Drop Down 1</a></li>
-                                <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
-                                    <ul>
-                                        <li><a href="#">Deep Drop Down 1</a></li>
-                                        <li><a href="#">Deep Drop Down 2</a></li>
-                                        <li><a href="#">Deep Drop Down 3</a></li>
-                                        <li><a href="#">Deep Drop Down 4</a></li>
-                                        <li><a href="#">Deep Drop Down 5</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Drop Down 2</a></li>
-                                <li><a href="#">Drop Down 3</a></li>
-                                <li><a href="#">Drop Down 4</a></li>
-                            </ul>
-                        </li>
-                        <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+                        <li><a className="nav-link scrollto active" href="/">Home</a></li>
+                        <li><a className="nav-link scrollto" href="/catalog">Comics catalog</a></li>
+                        <li><a className="nav-link scrollto" href="/create">Add comics</a></li>
+                        <li><a className="nav-link scrollto" href="/login">Login</a></li>
+                        <li><a className="nav-link scrollto" href="/register">Register</a></li>
+                        <li><a className="nav-link scrollto" href="/logout">Logout</a></li>
                     </ul>
                     <i className="bi bi-list mobile-nav-toggle"></i>
                 </nav>
