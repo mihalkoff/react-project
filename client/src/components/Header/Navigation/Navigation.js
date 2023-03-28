886bb48d7e169bbeddef6c7238d171f44efc2363
mihalkoff <ushuaya@abv.bg>
2023-03-28 21:48:42 +0300

    Update Navigation.js

--- client/src/components/Header/Navigation/Navigation.js
+++ client/src/components/Header/Navigation/Navigation.js
@@ -2,12 +2,12 @@ export const Navigation = () => {
     return (
         <nav id="navbar" className="navbar">
             <ul>
-                <li><a className="nav-link scrollto active" href="/">Home</a></li>
-                <li><a className="nav-link scrollto" href="/catalog">Comics catalog</a></li>
-                <li><a className="nav-link scrollto" href="/create">Add comics</a></li>
-                <li><a className="nav-link scrollto" href="/login">Login</a></li>
-                <li><a className="nav-link scrollto" href="/register">Register</a></li>
-                <li><a className="nav-link scrollto" href="/logout">Logout</a></li>
+                <li><a className="nav-link" href="/">Home</a></li>
+                <li><a className="nav-link" href="/catalog">Comics catalog</a></li>
+                <li><a className="nav-link" href="/create">Add comics</a></li>
+                <li><a className="nav-link" href="/login">Login</a></li>
+                <li><a className="nav-link" href="/register">Register</a></li>
+                <li><a className="nav-link" href="/logout">Logout</a></li>
             </ul>
             <i className="bi bi-list mobile-nav-toggle"></i>
         </nav>
