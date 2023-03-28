2f8b3f44e1b74f967af965f6b38c8410908e3bef
mihalkoff <ushuaya@abv.bg>
2023-03-28 20:16:53 +0300

    Create Catalog.js and css

---
+++ client/src/components/Catalog/Catalog.js
@@ -0,0 +1,53 @@
+import styles from './catalog.css';
+
+export const Catalog = () => {
+    return (
+        <section id="blog" className="padd-section">
+            <div className="container">
+                <div className="section-title text-center">
+                    <h2>Catalog</h2>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 col-lg-4">
+                        <div className="block-blog text-left">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/first-react-project-8439a.appspot.com/o/images%2F41QPSKs%2BR4L._SX258_BO1%2C204%2C203%2C200_.jpg?alt=media&amp;token=ff5de0cc-4d75-4060-90f2-29ad5b05e96a" className="img-responsive" alt="img"/>
+                            <div className="content-blog">
+                                <h4><a href="/details/5ekg12IO5WVQFL7bbeP8">The Rabbit Listened</a></h4>
+                                <a className="pull-right readmore" href="/details/5ekg12IO5WVQFL7bbeP8">read more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                        <div className="block-blog text-left">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/first-react-project-8439a.appspot.com/o/images%2F51sc93P5X8L.jpg?alt=media&amp;token=79d74935-7d00-4f39-9b87-e1d531959283" className="img-responsive" alt="img"/>
+                            <div className="content-blog">
+                                <h4><a href="/details/IpKRXN3p1xa4zb5yD7JH">The Tiger Who Came to Tea</a></h4>
+                                <a className="pull-right readmore" href="/details/IpKRXN3p1xa4zb5yD7JH">read more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                        <div className="block-blog text-left">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/first-react-project-8439a.appspot.com/o/images%2F51suSxpPVLL._AC_SY580_.jpg?alt=media&amp;token=f0453735-ebc9-4ac4-a5cc-26019d968e15" className="img-responsive" alt="img"/>
+                            <div className="content-blog">
+                                <h4><a href="/details/NosYmryvB4lEdJ1A7pC4">When I am Angry</a></h4>
+                                <a className="pull-right readmore" href="/details/NosYmryvB4lEdJ1A7pC4">read more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                        <div className="block-blog text-left">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/first-react-project-8439a.appspot.com/o/images%2F51QKrjevC2L._SY498_BO1%2C204%2C203%2C200_.jpg?alt=media&amp;token=5b6b2216-6b89-4745-9d91-b0843af289d8" className="img-responsive" alt="img"/>
+                            <div className="content-blog">
+                                <h4><a href="/details/REpC0wNFtLqbiFUDnPkF">When I Feel Frustrated</a></h4>
+                                <a className="pull-right readmore" href="/details/REpC0wNFtLqbiFUDnPkF">read more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
