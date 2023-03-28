57659091771d252fe392037cedb70394ae52bead
mihalkoff <ushuaya@abv.bg>
2023-03-28 13:32:12 +0300

    Create Register.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Home } from './components/Home/Home';
 import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
 				</Routes>
 			</main>
 			<Footer />
---
+++ client/src/components/Register/Register.js
@@ -0,0 +1,34 @@
+export const Register = () => {
+    return (
+        <section id="contact" className="padd-section">
+            <div className="container">
+                <div className="section-title text-center">
+                    <h2>Register</h2>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-lg-5 col-md-8">
+                        <div className="form">
+                            <form className="contactForm">
+                                <div className="form-group" style={{marginBottom: '1rem'}}>
+                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email"/>
+                                </div>
+                                <div className="form-group" style={{marginBottom: '1rem'}}>
+                                    <input type="password" autocomplete="on" className="form-control" name="password" id="register-password" placeholder="Password"/>
+                                </div>
+                                <div className="form-group" style={{marginBottom: '1rem'}}>
+                                    <input type="password" autocomplete="on" className="form-control" name="confirm-password" id="confirm-password" placeholder="Confirm Password"/>
+                                </div>
+                                <div className="text-center" style={{marginBottom: '1rem'}}>
+                                    <button className="btn btn-primary" type="submit">Register</button>
+                                </div>
+                            </form>
+                            <p>If you already have profile click <a href="/login">here</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
