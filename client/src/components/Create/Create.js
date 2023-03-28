57d0bcbe2fbb769ad9c90003064d4ac28d1efcf6
mihalkoff <ushuaya@abv.bg>
2023-03-28 21:48:37 +0300

    Update Create.js

--- client/src/components/Create/Create.js
+++ client/src/components/Create/Create.js
@@ -14,19 +14,19 @@ export const Create = () => {
                         <div className="form">
                             <form className="contactForm">
                                 <div className="form-group bottom-space">
-                                    <input type="text" name="title" className="form-control" id="name" placeholder="Title" data-rule="minlen:4" data-msg="Please enter at least 4 chars" value=""/>
+                                    <input type="text" name="title" className="form-control" id="name" placeholder="Title"/>
                                 </div>
                                 <div className="form-group bottom-space">
-                                    <input type="text" name="author" className="form-control" id="subject" placeholder="Author" data-rule="minlen:4" data-msg="Please enter at least 4 chars" value=""/>
+                                    <input type="text" name="author" className="form-control" id="author" placeholder="Author"/>
                                 </div>
                                 <div className="form-group bottom-space">
-                                    <input type="number" className="form-control" name="age" id="email" placeholder="Age" data-rule="minlen:1" data-msg="Please enter a valid age" value=""/>
+                                    <input type="number" className="form-control" name="universe" id="universe" placeholder="Universe"/>
                                 </div>
                                 <div className="form-group bottom-space">
-                                    <textarea className="form-control" name="description" rows="5" data-rule="required" data-msg="Please write something for the book" placeholder="Description"></textarea>
+                                    <textarea className="form-control" name="description" rows="5" placeholder="Description"></textarea>
                                 </div>
                                 <div className="text-center">
-                                    <button type="submit" className="btn btn-primary">Add</button>
+                                    <button type="submit" className="btn btn-primary">Add comics</button>
                                 </div>
                             </form>
                         </div>
