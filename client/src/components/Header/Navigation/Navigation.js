de67e0065f47e9752df5b6ac01d1cb43beba9d3a
mihalkoff <ushuaya@abv.bg>
2023-04-02 23:22:29 +0300

    Update Navigation.js with Link

--- client/src/components/Header/Navigation/Navigation.js
+++ client/src/components/Header/Navigation/Navigation.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 export const Navigation = () => {
     return (
         <nav id="navbar" className="navbar">
             <ul>
-                <li><a className="nav-link" href="/">Home</a></li>
-                <li><a className="nav-link" href="/catalog">Catalog</a></li>
-                <li><a className="nav-link" href="/create">Add comics</a></li>
-                <li><a className="nav-link" href="/login">Login</a></li>
-                <li><a className="nav-link" href="/register">Register</a></li>
-                <li><a className="nav-link" href="/logout">Logout</a></li>
+                <li><Link className="nav-link" to="/">Home</Link></li>
+                <li><Link className="nav-link" to="/catalog">Catalog</Link></li>
+                <li><Link className="nav-link" to="/create">Add comics</Link></li>
+                <li><Link className="nav-link" to="/login">Login</Link></li>
+                <li><Link className="nav-link" to="/register">Register</Link></li>
+                <li><Link className="nav-link" to="/logout">Logout</Link></li>
             </ul>
             <i className="bi bi-list mobile-nav-toggle"></i>
         </nav>
