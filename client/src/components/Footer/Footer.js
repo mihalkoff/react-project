0cbeb9b11a4364a8d84299e551ba618fac444ae1
mihalkoff <ushuaya@abv.bg>
2023-04-08 18:27:39 +0300

    Update Footer.js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export const Footer = () => {
     return (
         <footer className="footer">
@@ -6,59 +8,22 @@ export const Footer = () => {
 
                     <div className="col-md-12 col-lg-4">
                         <div className="footer-logo">
-                            <a className="navbar-brand" href="#">eStartup</a>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+                            <Link className="navbar-brand" to="/">Comics Store</Link>
+                            <p>Comics Store is the world’s largest and best-known science fiction, fantasy and cult entertainment retailer. We specialise in selling action figures, books, comics, graphic novels, toys and games and we offer all the best merchandise from your favourite series.</p>
                         </div>
                     </div>
 
                     <div className="col-sm-6 col-md-3 col-lg-2">
                         <div className="list-menu">
-                            <h4>Abou Us</h4>
+                            <h4>All links</h4>
                             <ul className="list-unstyled">
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Features item</a></li>
-                                <li><a href="#">Live streaming</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/catalog">Catalog</Link></li>
+                                <li><Link to="/login">Login</Link></li>
+                                <li><Link to="/register">Register</Link></li>
                             </ul>
                         </div>
                     </div>
-
-                    <div className="col-sm-6 col-md-3 col-lg-2">
-                        <div className="list-menu">
-                            <h4>Abou Us</h4>
-                            <ul className="list-unstyled">
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Features item</a></li>
-                                <li><a href="#">Live streaming</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div className="col-sm-6 col-md-3 col-lg-2">
-                        <div className="list-menu">
-                            <h4>Support</h4>
-                            <ul className="list-unstyled">
-                                <li><a href="#">faq</a></li>
-                                <li><a href="#">Editor help</a></li>
-                                <li><a href="#">Contact us</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div className="col-sm-6 col-md-3 col-lg-2">
-                        <div className="list-menu">
-                            <h4>Abou Us</h4>
-                            <ul className="list-unstyled">
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Features item</a></li>
-                                <li><a href="#">Live streaming</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
                 </div>
             </div>
 
@@ -66,7 +31,7 @@ export const Footer = () => {
                 <div className="container">
                     <p>&copy; Copyrights eStartup. All rights reserved.</p>
                     <div className="credits">
-                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                        Designed by <Link to="https://bootstrapmade.com/">BootstrapMade</Link>
                     </div>
                 </div>
             </div>
